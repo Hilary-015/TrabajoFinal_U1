62228bd53bbe05187fd4808c1e5e5aa5d1a5991b
Hilary Madeley Calva Camacho <105516501+Hilary-015@users.noreply.github.com>
2022-06-07 11:32:56 -0500

    Add files via upload

---
+++ HistorialComandos/src/Controlador/TDA_Cola/Cola.java
@@ -0,0 +1,64 @@
+package Controlador.TDA_Cola;
+
+import Controlador.Exceptions.PosicionException;
+import Controlador.Exceptions.TopeException;
+import Controlador.TDA_Lista.ListaEnlazada;
+
+/**
+ *
+ * @author Hilary Calva
+ */
+public class Cola<E> extends ListaEnlazada<E> {
+
+    private Integer tope;
+
+    public Cola(Integer pos) {
+        this.tope = pos;
+    }
+
+    public Cola() {
+        this.tope = 0;
+    }
+
+    public Boolean estaLleno() {
+        if (tope == 0) {
+            return false;
+        } else if (tope == getSize().intValue()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void queue(E dato) throws PosicionException, TopeException {
+        if (!estaLleno()) {
+            insertar(dato, getSize() - 1);
+        } else {
+            //Reemplazar dato cuando llegue al tope
+            dequeue(0);
+            insertar(dato, getSize() - 1);
+        }
+    }
+
+    public E dequeue(Integer pos) throws PosicionException, TopeException {
+        E auxDato = null;
+        if (!estaVacia()) {
+            if (pos >= 0 && pos < getSize()) {
+                for (int i = 0; i <= pos; i++) {
+                    auxDato = eliminarDato(0);
+                }
+                return auxDato;
+
+            } else {
+                throw new PosicionException("Error al realizar Dequeue: No existe la posicion dada");
+            }
+        } else {
+            throw new TopeException("La cola esta vacia");
+        }
+    }
+
+    public E obtenerDatoCola(Integer pos) throws PosicionException, TopeException {
+        return obtenerDato(pos);
+    }
+}
+
