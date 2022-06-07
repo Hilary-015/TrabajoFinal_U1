fd8f2e07436553821c933bb073a76ffde59040fb
Hilary Madeley Calva Camacho <105516501+Hilary-015@users.noreply.github.com>
2022-06-07 12:31:23 -0500

    Add files via upload

---
+++ TicketsBanco/src/Controlador/TicketsController/TicketDatos.java
@@ -0,0 +1,19 @@
+
+package Controlador.TicketsController;
+
+import Modelo.Ticket;
+
+/**
+ *
+ * @author Hilary Calva
+ */
+public class TicketDatos extends Ticket{
+
+    public TicketDatos(String tramite, int nro, int nroOficina) {
+        super(tramite, nro, nroOficina);
+    }
+
+    public TicketDatos() {
+    }
+
+}
